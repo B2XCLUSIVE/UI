c2a5fb23a5bf18ea56cbde99144127ea3aaaddf3
Fullstack Mechanic <fullstack@Fullstacks-MacBook-Air.local>
2024-03-17 01:58:34 +0100

    finished footer

--- app/page.jsx
+++ app/page.jsx
@@ -10,9 +10,24 @@ export default function Home() {
           Stories, Thoughts <br /> Ideas and More
         </h1>
 
-        <div>
-          <Link href={"#"}>Continue</Link>
-          <Link href={"#"}>Explore</Link>
+        <p className="text-center">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Libero, non
+          optio. Ipsa culpa quibusdam necessitatibus!
+        </p>
+
+        <div className="flex gap-4">
+          <Link
+            className="py-1 px-4 rounded-full border border-white"
+            href={"#"}
+          >
+            Continue
+          </Link>
+          <Link
+            className="py-1 px-4 rounded-full border border-white"
+            href={"#"}
+          >
+            Explore
+          </Link>
         </div>
       </section>
     </main>
