0fd32bf1fad193abd70823b3decdceb56846aab6
Fullstack Mechanic <fullstack@Fullstacks-MacBook-Air.local>
2024-03-17 02:19:49 +0100

    finished footer

--- app/page.jsx
+++ app/page.jsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import Link from "next/link";
+import { FaBook } from "react-icons/fa";
 
 export default function Home() {
   return (
@@ -28,6 +29,54 @@ export default function Home() {
           >
             Explore
           </Link>
+        </div>
+      </section>
+
+      <section className="md:w-[90%] mx-auto md:flex gap-4 p-4">
+        <div className="w-full h-48 relative">
+          <Image
+            src={"/pod.jpeg"}
+            className="w-full h-full object-cover"
+            width={1000}
+            height={1000}
+            alt="music"
+          />
+
+          <div className="absolute top-6 left-6 text-white flex flex-col gap-2">
+            <h1 className="font-bold text-2xl">Trending Topics</h1>
+            <p>Nigeria</p>
+            <FaBook className="text-2xl" />
+          </div>
+        </div>
+        <div className="w-full h-48 relative">
+          <Image
+            src={"/music.jpeg"}
+            className="w-full h-full object-cover"
+            width={1000}
+            height={1000}
+            alt="music"
+          />
+
+          <div className="absolute top-6 left-6 text-white flex flex-col gap-2">
+            <h1 className="font-bold text-2xl">Trending Topics</h1>
+            <p>Nigeria</p>
+            <FaBook className="text-2xl" />
+          </div>
+        </div>
+        <div className="w-full relative bg-black">
+          <Image
+            src={"/pod.jpeg"}
+            className="w-full h-48 object-cover"
+            width={1000}
+            height={1000}
+            alt="music"
+          />
+
+          <div className="absolute top-6 left-6 text-white flex flex-col gap-2">
+            <h1 className="font-bold text-2xl">Trending Topics</h1>
+            <p>Nigeria</p>
+            <FaBook className="text-2xl" />
+          </div>
         </div>
       </section>
     </main>
