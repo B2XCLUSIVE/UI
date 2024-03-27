04767734ffa9b19d24ccc72d708926406205f737
Fullstack Mechanic <fullstack@Fullstacks-MacBook-Air.local>
2024-03-27 10:35:52 +0100

    homepage is fine now

--- context/ThemeContext.jsx
+++ context/ThemeContext.jsx
@@ -4,11 +4,11 @@ import { createContext, useEffect, useState } from "react";
 export const ThemeContext = createContext();
 const getFromLocalStorage = () => {
   if (typeof window !== "undefined" && window.localStorage) {
-    const value = localStorage.getItem("theme");
-    return value || "light";
+    const value = localStorage.getItem("theme") || "light";
+    return value; // Return the retrieved value from local storage or fallback to "light"
+  } else {
+    return "light"; // Fallback in case localStorage is not available
   }
-
-  return "light"; // Fallback in case localStorage is not available
 };
 
 export const ThemeContextProvider = ({ children }) => {
