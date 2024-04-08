22cc4e506d3bca0d7106aca69b9195ef95b8a193
Fullstack Mechanic <fullstack@Fullstacks-MacBook-Air.local>
2024-04-08 16:47:42 +0100

    we

--- components/BlogPost.jsx
+++ components/BlogPost.jsx
@@ -10,8 +10,8 @@ function BlogPost() {
   const { theme } = useContext(ThemeContext);
   return (
     <>
-      <Link href={"blogs/1"} className={` flex`}>
-        <div className="p-4 flex flex-col gap-2 w-2/3">
+      <Link href={"blogs/1"} className={` flex `}>
+        <div className="p-4 flex flex-col gap-2 md:w-2/3 w-full">
           <div className="flex gap-2 items-center">
             <div className="w-[20px] h-[20px]">
               <Image
@@ -22,8 +22,14 @@ function BlogPost() {
                 className="w-full h-full object-cover rounded-full"
               />
             </div>
-            <p className={`${theme}-text`}>George Washnington</p>
-            <p className={`text-primarycolor font-bold`}>Follow</p>
+            <p className={`${theme}-text md:text-base text-[11px]`}>
+              George Washnington
+            </p>
+            <p
+              className={`text-primarycolor font-bold md:text-base text-[11px]`}
+            >
+              Follow
+            </p>
           </div>
           <h1 className={`${theme}-text text-sm md:text-xl font-bold`}>
             Rock &apos;n Roll Really Is The
@@ -31,9 +37,13 @@ function BlogPost() {
           <p className={`${theme}-text text-[10px] md:text-base`}>
             This is Photoshop&apos;s version of Lorem Ipsum.
           </p>
-          <div className="flex flex-col md:flex-row md:items-center gap-4">
-            <p className={`${theme}-text`}>March 6 2024</p>
-            <p className={`${theme}-text`}>6 Mins read</p>
+          <div className="flex  items-center md:gap-4 gap-2">
+            <p className={`${theme}-text md:text-base text-[11px]`}>
+              March 6 2024
+            </p>
+            <p className={`${theme}-text md:text-base text-[11px]`}>
+              6 Mins read
+            </p>
 
             <p className="text-white py-1 font-bold text-[10px]  px-3 bg-green-500 rounded-full">
               Trending
@@ -46,7 +56,7 @@ function BlogPost() {
             </Link>
           </div>
         </div>
-        <div className="w-1/3 h-[150px] md:h-[150px]">
+        <div className="hidden md:block w-1/3 h-[150px] md:h-[150px]">
           <Image
             src={"/alb.jpeg"}
             width={1000}
