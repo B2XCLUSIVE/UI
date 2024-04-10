9297cc19690f16d8c546d0c4d6d4c38b38bbe4ac
Fullstack Mechanic <fullstack@Fullstacks-MacBook-Air.local>
2024-04-10 11:02:53 +0100

    logs

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -57,23 +57,23 @@ function About() {
 
             <div>
               <h1 className={` ${theme}-text text-2xl`}> Musics</h1>
-              <p>
+              <p className={`${theme}-text`}>
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum,
                 aliquam?
               </p>
             </div>
 
             <div>
-              <h1 className="text-2xl"> Blogs</h1>
-              <p>
+              <h1 className={` ${theme}-text text-2xl`}> Blogs</h1>
+              <p className={`${theme}-text`}>
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum,
                 aliquam?
               </p>
             </div>
 
             <div>
-              <h1 className="text-2xl"> Events</h1>
-              <p>
+              <h1 className={` ${theme}-text text-2xl`}> Events</h1>
+              <p className={`${theme}-text`}>
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum,
                 aliquam?
               </p>
@@ -82,23 +82,23 @@ function About() {
           <div className="w-full md:w-2/4 flex flex-col gap-10">
             <div className="flex gap-20">
               <div className="flex flex-col items-center w-2/4">
-                <h1 className="font-bold text-4xl">20+</h1>
-                <p>Years in Business</p>
+                <h1 className={` ${theme}-text font-bold text-4xl`}>20+</h1>
+                <p className={`${theme}-text`}>Years in Business</p>
               </div>
 
               <div className="flex flex-col items-center w-2/4">
-                <h1 className="font-bold text-4xl">5000+</h1>
-                <p>Music and Video Plays</p>
+                <h1 className={` ${theme}-text font-bold text-4xl`}>5000+</h1>
+                <p className={`${theme}-text`}>Music and Video Plays</p>
               </div>
             </div>
             <div className="flex gap-20">
               <div className="flex flex-col items-center w-2/4">
-                <h1 className="font-bold text-4xl">500+</h1>
-                <p>Satisfied Users</p>
+                <h1 className={` ${theme}-text font-bold text-4xl`}>500+</h1>
+                <p className={`${theme}-text`}>Satisfied Users</p>
               </div>
               <div className="flex flex-col items-center w-2/4">
-                <h1 className="font-bold text-4xl">21</h1>
-                <p>Awards</p>
+                <h1 className={` ${theme}-text font-bold text-4xl`}>21</h1>
+                <p className={`${theme}-text`}>Awards</p>
               </div>
             </div>
           </div>
@@ -106,8 +106,8 @@ function About() {
 
         <section className="w-full p-2 md:p-8 md:w-5/6 md:mx-auto md:flex md:items-center md:gap-8">
           <div className="w-full md:w-2/4">
-            <h1 className="font-bold text-3xl">Our Vision</h1>
-            <p>
+            <h1 className={` ${theme}-text font-bold text-3xl`}>Our Vision</h1>
+            <p className={`${theme}-text`}>
               Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ut
               asperiores repudiandae in dolor consequuntur cum maiores eos,
               corporis officia assumenda perspiciatis, excepturi aliquid ab nam,
@@ -123,8 +123,8 @@ function About() {
             </p>
           </div>
           <div className="w-full md:w-2/4">
-            <h1 className="font-bold text-3xl">Our Mission</h1>
-            <p>
+            <h1 className={` ${theme}-text font-bold text-3xl`}>Our Mission</h1>
+            <p className={`${theme}-text`}>
               Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ut
               asperiores repudiandae in dolor consequuntur cum maiores eos,
               corporis officia assumenda perspiciatis, excepturi aliquid ab nam,
