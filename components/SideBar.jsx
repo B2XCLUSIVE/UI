86ba1c053ce7add852d745e56214927d6c3655b4
Fullstack Mechanic <fullstack@Fullstacks-MacBook-Air.local>
2024-04-15 14:32:04 +0100

    sidebar is responsive

--- components/SideBar.jsx
+++ components/SideBar.jsx
@@ -1,7 +1,7 @@
 "use client";
 
 const { ThemeContext } = require("@/context/ThemeContext");
-const { useContext } = require("react");
+const { useContext, useState } = require("react");
 const {
   FaArrowLeft,
   FaChartBar,
@@ -11,66 +11,87 @@ const {
   FaChartPie,
   FaNotesMedical,
   FaTools,
+  FaHamburger,
 } = require("react-icons/fa");
 import Link from "next/link";
 function SideBar() {
   const { theme } = useContext(ThemeContext);
+
+  const [showSideBar, setShowSideBar] = useState(false);
+
   return (
     <>
-      <div className={`border-r border-gray-500 p-4 w-2/12  `}>
-        <div className="flex justify-between">
-          <h1 className={`${theme}-text font-bold`}>Menu</h1>
+      <div
+        className={`border-r border-gray-500 p-4   ${showSideBar ? "w-2/12" : "w-[70px]"} `}
+      >
+        <div className={`flex ${showSideBar ? "p-4" : "p-2"} justify-between`}>
+          <h1
+            className={`${theme}-text font-bold ${showSideBar ? "block" : "hidden"}`}
+          >
+            Menu
+          </h1>
 
-          <FaArrowLeft className={`${theme}-text`} />
+          <FaHamburger
+            className={`${theme}-text cursor-pointer text-xl `}
+            onClick={() => setShowSideBar(!showSideBar)}
+          />
         </div>
 
         <div className="py-4">
           <Link
             href={"/overview"}
-            className={`${theme}-text flex p-4 hover:bg-primarycolor items-center gap-2`}
+            className={`${theme}-text flex ${showSideBar ? "p-4" : "p-2"} hover:bg-primarycolor rounded-lg items-center gap-2  `}
           >
-            <FaChartBar />
-            Dashboard
+            <FaChartBar className="text-3xl" />
+            <p className={` ${showSideBar ? "block" : "hidden"}  `}>
+              Dashboard
+            </p>
           </Link>
 
           <Link
-            href={"/analytics"}
-            className={`${theme}-text flex p-4 hover:bg-primarycolor items-center gap-2`}
+            href={"/overview"}
+            className={`${theme}-text flex ${showSideBar ? "p-4" : "p-2"} hover:bg-primarycolor rounded-lg items-center gap-2  `}
           >
-            <FaChartPie />
-            Analytics
+            <FaChartPie className="text-3xl" />
+            <p className={` ${showSideBar ? "block" : "hidden"}  `}>
+              Analytics
+            </p>
           </Link>
 
           <Link
-            href={"/contents"}
-            className={`${theme}-text flex p-4 hover:bg-primarycolor items-center gap-2`}
+            href={"/overview"}
+            className={`${theme}-text flex ${showSideBar ? "p-4" : "p-2"} hover:bg-primarycolor rounded-lg items-center gap-2  `}
           >
-            <FaCalendar />
-            Content
+            <FaCalendar className="text-3xl" />
+            <p className={` ${showSideBar ? "block" : "hidden"}  `}>Content</p>
           </Link>
 
           <Link
-            href={"/followers"}
-            className={`${theme}-text flex p-4 hover:bg-primarycolor items-center gap-2`}
+            href={"/overview"}
+            className={`${theme}-text flex ${showSideBar ? "p-4" : "p-2"} hover:bg-primarycolor rounded-lg items-center gap-2  `}
           >
-            <FaUser />
-            Followers
+            <FaUser className="text-3xl" />
+            <p className={` ${showSideBar ? "block" : "hidden"}  `}>
+              Followers
+            </p>
           </Link>
 
           <Link
-            href={"/createpost"}
-            className={`${theme}-text flex p-4 hover:bg-primarycolor items-center gap-2`}
+            href={"/overview"}
+            className={`${theme}-text flex ${showSideBar ? "p-4" : "p-2"} hover:bg-primarycolor rounded-lg items-center gap-2  `}
           >
-            <FaNotesMedical />
-            Create Post
+            <FaNotesMedical className="text-3xl" />
+            <p className={` ${showSideBar ? "block" : "hidden"}  `}>
+              Create Post
+            </p>
           </Link>
 
           <Link
-            href={"/account"}
-            className={`${theme}-text flex p-4 hover:bg-primarycolor items-center gap-2`}
+            href={"/overview"}
+            className={`${theme}-text flex ${showSideBar ? "p-4" : "p-2"} hover:bg-primarycolor rounded-lg items-center gap-2  `}
           >
-            <FaTools />
-            Account
+            <FaTools className="text-3xl" />
+            <p className={` ${showSideBar ? "block" : "hidden"}  `}>Account</p>
           </Link>
         </div>
       </div>
