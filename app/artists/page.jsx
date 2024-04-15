2708ba0360170b12625015e8a64d1ec8ded5fd06
Fullstack Mechanic <fullstack@Fullstacks-MacBook-Air.local>
2024-04-15 13:34:32 +0100

    adding search

--- app/artists/page.jsx
+++ app/artists/page.jsx
@@ -4,7 +4,7 @@ import SectionHeader from "@/components/SectionHeader";
 import { ThemeContext } from "@/context/ThemeContext";
 import Image from "next/image";
 import { useContext } from "react";
-import { FaBackward, FaForward, FaPlus } from "react-icons/fa";
+import { FaBackward, FaForward, FaPlus, FaSearch } from "react-icons/fa";
 
 function Artists() {
   const { theme } = useContext(ThemeContext);
@@ -23,6 +23,33 @@ function Artists() {
             Lorem ipsum dolor sit amet consectetur, adipisicing elit. Illum,
             consequatur.
           </p>
+        </div>
+      </section>
+
+      <section className="w-full md:w-5/6 mx-auto p-4">
+        <div className="flex gap-4 w-full">
+          <div className="md:w-4/6 rounded-full flex items-center z-10 border border-white">
+            <input
+              type="text"
+              placeholder="Search here"
+              className={` ${theme}-text w-11/12 bg-transparent p-4 text-white outline-none `}
+            />
+            <button className="rounded-full bg-primarycolor flex items-center md:text-base text-[12px] py-2 gap-1 px-4 mr-2">
+              <FaSearch /> Search
+            </button>
+          </div>
+
+          <div className="w-2/6 p-4 bg-transparent outline-none border rounded-lg">
+            <select
+              name=""
+              id=""
+              className="w-full bg-transparent outline-none"
+            >
+              <option value="">Filter</option>
+              <option value="">Realesed Date</option>
+              <option value="">Artist</option>
+            </select>
+          </div>
         </div>
       </section>
       <section
