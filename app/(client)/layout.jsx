61142db89f98dfc1d999817864d5cdc1c8b617a7
Fullstack Mechanic <fullstack@Fullstacks-MacBook-Air.local>
2024-05-23 14:12:57 +0100

    fixed

--- app/(client)/layout.jsx
+++ app/(client)/layout.jsx
@@ -1,12 +1,10 @@
 import Footer from "@/components/Footer";
 import Header from "@/components/Header";
-import ToastNotificationContainer from "@/components/ToastNotificationComponent";
 
 function Layout({ children }) {
   return (
     <>
       <Header />
-      <ToastNotificationContainer />
       {children}
       <Footer />
     </>
