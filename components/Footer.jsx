0dc54c8c1ec82ce68bc300e480f4cf7ab0483774
fullstackmechanic <sirhmvfx@gmail.com>
2024-09-01 15:37:05 +0100

    ui

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -16,31 +16,7 @@ function Footer() {
   const { theme } = useContext(ThemeContext);
   return (
     <>
-      <div className={`${theme}-bgg p-8 flex flex-col gap-3`}>
-        <h1 className={` ${theme}-text   md:text-3xl font-bold text-center`}>
-          B2EXCLUSIVE
-        </h1>
-
-        <div className="flex justify-center items-center gap-4">
-          <Link href={"#"} className={`${theme}-text text-sm`}>
-            <FaFacebook />
-          </Link>
-          <Link href={"#"} className={`${theme}-text text-sm`}>
-            <FaTwitter />
-          </Link>
-          <Link href={"#"} className={`${theme}-text text-sm`}>
-            <FaLinkedin />
-          </Link>
-          <Link href={"#"} className={`${theme}-text text-sm`}>
-            <FaYoutube />
-          </Link>
-          <Link href={"#"} className={`${theme}-text text-sm`}>
-            <FaSoundcloud />
-          </Link>
-        </div>
-      </div>
-
-      <div className="bg-primarycolor p-4 ">
+      <div className="bg-primarycolor p-20 ">
         <div className="flex flex-col items-center">
           <p>© 2024 B2EXCLUSIVE. All Right Reserved. </p>
           <div className="flex">
